4ac7f8761135f9ddbf850798ba86bf7682807f22
Jan Maack Kjerbye <50995332+janhalen@users.noreply.github.com>
2023-09-14 14:17:20 +0200

    Added strikethrough

--- .github/workflows/issue_to_markdown.py
+++ .github/workflows/issue_to_markdown.py
@@ -1,15 +1,11 @@
-import json
-import re
-
 def evaluate_checkmarks(section):
     # This function checks if all checkmarks in a section are marked
     checkmarks = re.findall(r'- \[[xX ]\]', section)
-    # If there are no checkmarks in the section, return False
+    # If there are no checkmarks in the section, return None
     if not checkmarks:
-        return False
+        return None
     # If there are checkmarks, check if all are marked
     return all(mark == '- [x]' or mark == '- [X]' for mark in checkmarks)
-
 
 def main():
     # Load the GitHub event data
@@ -26,8 +22,16 @@ def main():
 
     # Evaluate each section and modify the headers if necessary
     for i in range(1, len(sections), 2):
-        if evaluate_checkmarks(sections[i+1]):
+        evaluation = evaluate_checkmarks(sections[i+1])
+        if evaluation is None:
+            continue
+        elif evaluation:
             sections[i] = sections[i].rstrip() + ' COMPLIANT\n'
+        else:
+            # Apply strikethrough to the header and the following input section
+            sections[i] = '~~' + sections[i].rstrip() + '~~\n'
+            if i+2 < len(sections):
+                sections[i+2] = '~~' + sections[i+2] + '~~'
 
     # Join the sections back together
     new_issue_body = ''.join(sections)
