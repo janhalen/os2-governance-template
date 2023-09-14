acd9d280507550bbadfb765ccf414f924b472a65
Jan Maack Kjerbye <50995332+janhalen@users.noreply.github.com>
2023-09-14 13:41:08 +0200

    Rewrite of python script from scratch

--- .github/workflows/issue_to_markdown.py
+++ .github/workflows/issue_to_markdown.py
@@ -1,27 +1,35 @@
 import json
 import re
 
-# Load the issue body from the GitHub event
-with open('event.json') as f:
-    data = json.load(f)
-    issue_body = data['event']['issue']['body']
+def evaluate_checkmarks(section):
+    # This function checks if all checkmarks in a section are marked
+    checkmarks = re.findall(r'- \[[xX ]\]', section)
+    return all(mark == '- [x]' or mark == '- [X]' for mark in checkmarks)
 
-# Split the issue body into sections by headers
+def main():
+    # Load the GitHub event data
+    with open('event.json', 'r') as f:
+        event_data = json.load(f)
+
+    # Get the issue data
+    issue_data = event_data['issue']
+    issue_title = issue_data['title']
+    issue_body = issue_data['body']
+
+    # Split the issue body into sections
     sections = re.split(r'(### .*\n)', issue_body)
 
-# Process each section
-for i in range(1, len(sections), 2):
-    # Check if all checkboxes are checked
-    if re.search(r'- \[[xX ]\]', sections[i]):
-    # Not compliant, apply strikethrough to the section header
-        sections[i-1] = '~~' + sections[i-1].rstrip() + '~~\n'
-    else:
-    # Compliant, add "Compliant" to the section header
-        sections[i-1] = sections[i-1].rstrip() + ' (Compliant)\n'
+    # Evaluate each section and modify the headers if necessary
+    for i in range(1, len(sections), 2):
+        if evaluate_checkmarks(sections[i+1]):
+            sections[i] = sections[i].rstrip() + ' COMPLIANT\n'
 
-# Join the sections back together
+    # Join the sections back together
     new_issue_body = ''.join(sections)
 
-# Write the new issue body to a markdown file
-with open('issue.md', 'w') as f:
-    f.write(new_issue_body)
+    # Write to a Markdown file
+    with open(f'{issue_title}.md', 'w') as f:
+        f.write(f'# {issue_title}\n\n{new_issue_body}')
+
+if __name__ == "__main__":
+    main()
