1e79acb00e82e56c6bd257b91141359d85bf655c
Jan Maack Kjerbye <50995332+janhalen@users.noreply.github.com>
2023-09-14 14:32:59 +0200

    split logic into functions

--- .github/workflows/issue_to_markdown.py
+++ .github/workflows/issue_to_markdown.py
@@ -2,44 +2,41 @@ import json
 import re
 
 def evaluate_checkmarks(section):
-    # This function checks if all checkmarks in a section are marked
     checkmarks = re.findall(r'- \[[xX ]\]', section)
-    # If there are no checkmarks in the section, return None
     if not checkmarks:
         return None
-    # If there are checkmarks, check if all are marked
     return all(mark == '- [x]' or mark == '- [X]' for mark in checkmarks)
 
+def apply_strikethrough(section):
+    return '~~' + section + '~~'
+
+def add_compliant(section):
+    return section.rstrip() + ' COMPLIANT\n'
+
 def main():
-    # Load the GitHub event data
     with open('event.json', 'r') as f:
         event_data = json.load(f)
 
-    # Get the issue data from the 'event' object
     issue_data = event_data['event']['issue']
     issue_title = issue_data['title']
     issue_body = issue_data['body']
 
-    # Split the issue body into sections
     sections = re.split(r'(### .*\n)', issue_body)
 
-    # Evaluate each section and modify the headers if necessary
     for i in range(1, len(sections), 2):
         evaluation = evaluate_checkmarks(sections[i+1])
         if evaluation is None:
             continue
         elif evaluation:
-            sections[i] = sections[i].rstrip() + ' COMPLIANT\n'
+            sections[i] = add_compliant(sections[i])
         else:
-            # Apply strikethrough to the header and the following input section
-            sections[i] = '~~' + sections[i].rstrip() + '~~\n'
+            sections[i] = apply_strikethrough(sections[i].rstrip() + '\n')
+            sections[i+1] = apply_strikethrough(sections[i+1])
             if i+2 < len(sections):
-                sections[i+2] = '~~' + sections[i+2] + '~~'
+                sections[i+2] = apply_strikethrough(sections[i+2])
 
-    # Join the sections back together
     new_issue_body = ''.join(sections)
 
-    # Write to a Markdown file
     with open(f'{issue_title}.md', 'w') as f:
         f.write(f'# {issue_title}\n\n{new_issue_body}')
 
